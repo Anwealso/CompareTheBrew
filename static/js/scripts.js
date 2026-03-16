82e73d97eda17f2c110fcf4d035aa40c9d466011
Anwealso <48880195+Anwealso@users.noreply.github.com>
2026-03-17 00:39:50 +1100

    Unmucked a lot more styling

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function (e) {
-  document.getElementById("slide").addEventListener("click", function () {
-    document.getElementById("cover").classList.toggle("shadow");
-    document.getElementById("cover").classList.toggle("clickable");
-  });
+  // document.getElementById("slide").addEventListener("click", function () {
+  //   document.getElementById("cover").classList.toggle("shadow");
+  //   document.getElementById("cover").classList.toggle("clickable");
+  // });
 
   document.getElementById("cover").addEventListener("click", function () {
     document.getElementById("slide").checked = false;
